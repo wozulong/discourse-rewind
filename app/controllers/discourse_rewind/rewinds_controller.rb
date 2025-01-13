193f92c89ba1d85c52d3fcaba4876ba4815601b2
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2025-01-13 18:17:02 +0100

    requires_login

--- app/controllers/discourse_rewind/rewinds_controller.rb
+++ app/controllers/discourse_rewind/rewinds_controller.rb
@@ -4,6 +4,8 @@ module ::DiscourseRewind
   class RewindsController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
+    requires_login
+
     def show
       DiscourseRewind::Rewind::Fetch.call(service_params) do
         on_model_not_found(:year) { raise Discourse::NotFound }
