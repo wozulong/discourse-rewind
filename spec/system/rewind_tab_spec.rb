cdf6f0d0e713b7d75d89bbebe77312c48459ad87
Jarek Radosz <jarek@cvx.dev>
2025-10-09 14:53:37 +0200

    DEV: Update linting (#23)

--- spec/system/rewind_tab_spec.rb
+++ spec/system/rewind_tab_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe "DiscourseRewind | rewind tab", type: :system do
-  fab!(:current_user) { Fabricate(:user) }
+  fab!(:current_user, :user)
 
   before do
     SiteSetting.discourse_rewind_enabled = true
